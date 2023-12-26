dc0a4b0e69f90a2e12c701c39586226f01099ef0
yannis300307 <yannis300307@gmail.com>
2023-12-26 17:18:19 +0100

    Déplacement des exemples dans main.py

--- src/engine.py
+++ src/engine.py
@@ -33,37 +33,6 @@ class Engine:
         self.camera = Camera()
         self.entity_manager = EntityManager()
 
-        # TODO : REMOVE (ONLY USED FOR TESTING)
-
-        self.map_manager.load_new("maps/map2.tmj")
-
-        self.renderer.load_tile_set("assets/tiles.png", 16)
-
-        anim = Anim(0.5)
-        anim.load_animation_from_directory("assets/entities/player/none")
-        self.renderer.register_animation(anim, "player_none")
-
-        player = self.entity_manager.register_entity("player")
-        player.link_animation("player_none")
-        player.collision_rect = [-7, -7, 7, 7]
-
-        player.set_default_life(10)
-
-        self.camera.follow_entity(player)
-
-        for i in range(20):
-            anim = Anim(0.5)
-            anim.load_animation_from_directory("assets/entities/player/none")
-            self.renderer.register_animation(anim, f"player_none_{i}")
-
-            test = self.entity_manager.register_entity(f"test_{i}")
-            test.x = random.randint(0, 200)
-            test.y = random.randint(0, 200)
-            test.link_animation(f"player_none_{i}")
-            test.collision_rect = [-7, -7, 7, 7]
-
-            test.set_default_life(10)
-
     def loop(self):
         """Fonction à lancer au début du programme et qui va lancer les updates dans une boucle.
         Attend jusqu'à la fin du jeu."""
--- src/main.py
+++ src/main.py
@@ -1,4 +1,40 @@
+import random
+
+from src.animation import Anim
 from src.engine import Engine
 
-engine = Engine()
-engine.loop()
+
+class Game(Engine):
+    def __init__(self):
+        super().__init__()
+        self.map_manager.load_new("maps/map2.tmj")
+
+        self.renderer.load_tile_set("assets/tiles.png", 16)
+
+        anim = Anim(0.5)
+        anim.load_animation_from_directory("assets/entities/player/none")
+        self.renderer.register_animation(anim, "player_none")
+
+        player = self.entity_manager.register_entity("player")
+        player.link_animation("player_none")
+        player.collision_rect = [-7, -7, 7, 7]
+
+        player.set_default_life(10)
+
+        self.camera.follow_entity(player)
+
+        for i in range(20):
+            anim = Anim(0.5)
+            anim.load_animation_from_directory("assets/entities/player/none")
+            self.renderer.register_animation(anim, f"player_none_{i}")
+
+            test = self.entity_manager.register_entity(f"test_{i}")
+            test.x = random.randint(0, 200)
+            test.y = random.randint(0, 200)
+            test.link_animation(f"player_none_{i}")
+            test.collision_rect = [-7, -7, 7, 7]
+
+            test.set_default_life(10)
+
+game = Game()
+game.loop()
