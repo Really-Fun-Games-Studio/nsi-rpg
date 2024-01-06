910ef1039bdc4440374c92766db99f259122e465
yannis300307 <yannis300307@gmail.com>
2024-01-06 18:41:39 +0100

    Correction de fautes dans des commentaires

--- src/engine/event_sheduler.py
+++ src/engine/event_sheduler.py
@@ -17,14 +17,14 @@ class EventSheduler:
 
     @staticmethod
     def get_collisions_with_entity(rect: tuple[int, int, int, int], entity: 'Entity'):
-        """Retourne True si l'entité donnée touche le rectange donné."""
+        """Retourne True si l'entité donnée touche le rectangle donné."""
         return (rect[0] <= entity.x+entity.collision_rect[2] and
                 rect[0] + rect[2] >= entity.x+entity.collision_rect[0] and
                 rect[1] + rect[3] >= entity.y+entity.collision_rect[1] and
                 rect[1] <= entity.y+entity.collision_rect[3])
 
     def update(self):
-        """Met à jour l'event sheluder et execute les actions si les conditions à son execution sont respéctées."""
+        """Met à jour l'event sheduler et execute les actions si les conditions à son execution sont respéctées."""
 
         # On itère dans la liste des zones de détection
         for area in self.area_callbacks.copy():
