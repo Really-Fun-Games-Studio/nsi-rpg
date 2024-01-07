0f617c409b08956b7eb3c6dbfb4306d9d35f6726
Yannis300307 <yannis300307@gmail.com>
2024-01-07 11:47:53 +0100

    Ajout du premier menu et du premier widget

--- src/engine/menu_manager.py
+++ src/engine/menu_manager.py
@@ -1,4 +1,37 @@
+class Widget:
+    """Classe parente des widgets de menu."""
+    def __init__(self, x, y, is_window_relative):
+        self.x = x
+        self.y = y
+        self.is_window_relative = is_window_relative
+
+
+class Label(Widget):
+    """Un widget de texte."""
+    def __init__(self, x: int | float, y: int | float, text: str, size: int | float, centered: bool = False,
+                 is_window_relative: bool = -1):
+        super().__init__(x, y, is_window_relative)
+        self.text = text
+        self.size = size
+        self.centered = centered
+
+
+class Menu:
+    """Un menu contenant des widgets."""
+    def __init__(self):
+        self.widgets: list[Widget] = []
+
+    def add_widget(self, widget: Widget):
+        """Ajoute le widget donné au menu."""
+        self.widgets.append(widget)
+
+
 class MenuManager:
     """Classe qui gère les menus."""
+
     def __init__(self):
-        pass
+        self.menus = {}
+
+    def register_menu(self, menu: Menu, name: str):
+        """Ajoute le menu donné au manager de menu avec le nom donné."""
+        self.menus[name] = menu
