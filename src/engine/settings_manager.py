c625e15d34d7b44334a5f2f897b377bc52830aee
adastram <caudron.kerrian@gmail.com>
2024-01-09 21:24:37 +0100

    Added global sound volume managing

--- src/engine/settings_manager.py
+++ src/engine/settings_manager.py
@@ -1,11 +1,20 @@
 class Settings:
     def __init__(self) -> None:
         self.master_volume = 60
-        self.music_volume = 100
+        self.sound_master_volume = 100
+        self.music_master_volume = 100
+        self.global_sound_master_volume = 100
 
         self.zoom = 1.75
     
 
-    def get_music_volume(self):
-        return self.master_volume / 100 * self.music_volume
+    def get_music_master_volume(self):
+        return round(self.master_volume / 100 * self.music_master_volume, 3)
     
+    def get_global_sound_master_volume(self):
+        return round(self.master_volume / 100 * self.global_sound_master_volume, 3)
+    
+    def get_sound_master_volume(self):
+        return round(self.master_volume / 100 * self.sound_master_volume, 3)
+
+
