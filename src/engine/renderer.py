866d964c2574985b7c0d61fd2752822fda9602d4
yannis300307 <yannis300307@gmail.com>
2024-01-14 20:07:19 +0100

    Début de rendu de la vie

--- src/engine/renderer.py
+++ src/engine/renderer.py
@@ -488,6 +488,11 @@ class Renderer:
     def render_boss_fight_scene(self, delta: float):
         """Rend les sprites du joueur et du boss lors d'un combat de boss."""
 
+        # On rend le fond qui correspond au points des 2 concurents
+        total_point = self.engine.boss_fight_manager.player_points + self.engine.boss_fight_manager.boss_points
+        draw.rect(self.window, (75, 50, 255), (0, 0, math.ceil(display.get_window_size()[0]*(self.engine.boss_fight_manager.player_points/total_point)), display.get_window_size()[1]))
+
+
         # On récupère l'image de l'animation du boss
         boss_animation: Anim = self.boss_fight_boss_animations[self.engine.boss_fight_manager.current_boss_animation]
         frame = boss_animation.get_frame(delta)
