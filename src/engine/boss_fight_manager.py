1ec0a83f8325d712a157ca5a24f13e3c3d338cc7
adastram <caudron.kerrian@gmail.com>
2024-01-17 22:18:32 +0100

    Added dynamic temple order and conversation

--- src/engine/boss_fight_manager.py
+++ src/engine/boss_fight_manager.py
@@ -21,6 +21,16 @@ class BossFightManager:
         self.player_points = -1
         self.boss_points = -1
 
+        self.keys = 0
+
+        self.rick_number = 10
+        self.rick_anim_dict: dict[Anim] = {}
+
+        for i in range(self.rick_number):
+            anim = Anim(0.1)
+            anim.load_animation_from_directory("assets/textures/entities/rick")
+            self.rick_anim_dict[i] = anim
+
     def update(self):
         """Met à jour le combat de boss."""
         if self.engine.game_state == GameState.BOSS_FIGHT:
@@ -60,26 +70,30 @@ class BossFightManager:
         # Partie à retirer plus tard V
         match self.current_fight_id:
             case 1:
-                self.engine.dialogs_manager.start_dialog("temple_1", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                self.keys +=1
+                self.engine.dialogs_manager.start_dialog(f"temple_{self.keys}", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
             case 2:
-                self.engine.dialogs_manager.start_dialog("temple_2", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                self.keys +=1
+                self.engine.dialogs_manager.start_dialog(f"temple_{self.keys}", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                
             case 3:
-                self.engine.dialogs_manager.start_dialog("temple_3", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                self.keys +=1
+                self.engine.dialogs_manager.start_dialog(f"temple_{self.keys}", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                
             case 4:
-                self.engine.dialogs_manager.start_dialog("temple_4", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(self.final_temple_end)))
+                self.keys +=1
+                self.engine.dialogs_manager.start_dialog(f"temple_{self.keys}", lambda: self.engine.renderer.fadeout(2, (0, 0, 0), callback=lambda : self.finish_fight(None)))
+                
 
     def finish_fight(self, callback):
         """Finie le combat."""
         # Change la musique
-        self.engine.sound_manager.music_pause(0)
+        self.engine.sound_manager.music_stop_playlist()
         self.engine.sound_manager.music_remove_from_playlist(".\\assets\\OST\\boss_fight_1.mp3")
+        self.engine.sound_manager.music_next()
         self.engine.sound_manager.music_add_to_playlist(".\\assets\\OST\\forest_sound.mp3")
         self.engine.sound_manager.music_start_playlist()
-        volume = self.engine.sound_manager.music_get_volume()
-        self.engine.sound_manager.music_set_volume(0)
-        self.engine.sound_manager.music_resume(0)
-        self.engine.sound_manager.music_next()
-        self.engine.sound_manager.music_set_volume(volume)
+
 
         self.engine.entity_manager.resume()
 
@@ -87,17 +101,19 @@ class BossFightManager:
 
         if callback is not None:
             callback()
+            
+        if self.keys == 4:
+            self.final_temple_end()
 
     def final_temple_end(self):
         self.engine.dialogs_manager.start_dialog("out_temple_4")
         self.engine.event_sheduler.register_area((591, 358, 98, 46), lambda _: self.engine.dialogs_manager.start_dialog("before_door_open", self.end_game), ["player"], True)
 
     def end_game(self, *_):
-        for i in range(10):
-            anim = Anim(0.1)
-            anim.load_animation_from_directory("assets/textures/entities/rick")
+        for i in range(self.rick_number):
+            
             entity = self.engine.entity_manager.register_entity(f"rick_{i}")
-            self.engine.renderer.register_animation(anim, f"rick_anim_{i}")
+            self.engine.renderer.register_animation(self.rick_anim_dict[i], f"rick_anim_{i}")
             entity.link_animation(f"rick_anim_{i}")
             entity.max_speed = 64.0
             entity.x = 640.+random.randint(-40, 40)
@@ -105,10 +121,9 @@ class BossFightManager:
             entity.set_ai(RickAI, self.engine)
 
             # Change la musique
-            self.engine.sound_manager.music_pause(0)
+            self.engine.sound_manager.music_stop_playlist()
+            for music in self.engine.sound_manager.music_playlist:
+                self.engine.sound_manager.music_remove_from_playlist(music)
             self.engine.sound_manager.music_add_to_playlist(".\\assets\\OST\\rickrick.mp3")
-            self.engine.sound_manager.music_start_playlist()
-            volume = self.engine.sound_manager.music_get_volume()
-            self.engine.sound_manager.music_resume(0)
             self.engine.sound_manager.music_next()
-            self.engine.sound_manager.music_set_volume(volume)
+            self.engine.sound_manager.music_start_playlist()
